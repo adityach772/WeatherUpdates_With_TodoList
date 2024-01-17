c7ee053e556ea0a4e5c79d79838cbd3c2a0cde7a
aditya-772 <adityachundims@gmail.com>
2024-01-17 11:23:15 -0800

    Add project files.

---
+++ Data/Migrations/20240116042330_Inital_Setup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Data.Migrations
+{
+    public partial class Inital_Setup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "List",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Task = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Priority = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_List", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "List");
+        }
+    }
+}
---
+++ Models/List.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoList.Models
+{
+    public class List
+    {
+        public int Id { get; set; }
+        public string? Date { get; set; }
+        public string? Task { get; set; }
+
+        public string? Priority { get; set; }
+
+        public List()
+        {
+
+        }
+    }
+}
